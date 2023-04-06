0f3ceb9be7286c4b67cf30a0d8af809fbc685065
Subhash Shrestha <subhashshrestha3@gmail.com>
2023-04-06 09:48:45 +0930

    inital commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import { Provider } from 'react-redux'
+import { store } from '../app/store'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
+
+export default MyApp
