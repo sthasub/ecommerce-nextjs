2e102ea8b0c2f9adf9f3bf0eac8a5ffe79d9a1f1
Subhash Shrestha <subhashshrestha3@gmail.com>
2023-04-11 22:01:40 +0930

    change in secret name

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -11,7 +11,7 @@ export const authOptions = {
     // }),
     GoogleProvider({
         clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
+        clientSecret: process.env.SECRET,
     }),
     
     // ...add more providers here
