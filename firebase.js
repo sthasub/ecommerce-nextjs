80e000bcb4af7fe2970d7e2b04df9d623e3b3f58
Subhash Shrestha <subhashshrestha3@gmail.com>
2023-04-15 18:34:32 +0930

    changes in codes

--- firebase.js
+++ firebase.js
@@ -1,19 +1,37 @@
-import firebase from "firebase/compat/app";
-import 'firebase/compat/firestore';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import * as firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore";
 
-const firebaseConfig = {
+// import {initializeApp, applicationDefault, cert } from "firebase-admin/app";
+// import {getFirestore,Timestamp, FieldValue } from "firebase-admin/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCh-TQC0r6hvf6sfQlznuXM5Qqhvv4SoZU",
+//   authDomain: "ecommerce-nextjs-5b907.firebaseapp.com",
+//   projectId: "ecommerce-nextjs-5b907",
+//   storageBucket: "ecommerce-nextjs-5b907.appspot.com",
+//   messagingSenderId: "947716126382",
+//   appId: "1:947716126382:web:9db9df34851ade353b5f3f"
+// };
+
+// console.log(firebase.apps.length," FIREBASE ");
+// const app = firebase.apps.length != 0 ? firebase.initializeApp(firebaseConfig):firebase.app;
+// const db = app.storage()
+
+// export default async function initializeAppWithConfig(){
+//   initializeApp(firebaseConfig);
+//   const db= getFirestore();
+//   return db;  
+// }
+
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyCh-TQC0r6hvf6sfQlznuXM5Qqhvv4SoZU",
   authDomain: "ecommerce-nextjs-5b907.firebaseapp.com",
   projectId: "ecommerce-nextjs-5b907",
   storageBucket: "ecommerce-nextjs-5b907.appspot.com",
   messagingSenderId: "947716126382",
   appId: "1:947716126382:web:9db9df34851ade353b5f3f"
-};
-
-// const app = firebase.apps.length != 0 ? firebase.initializeApp(firebaseConfig):firebase.app();
-
-const db = firebase.initializeApp(firebaseConfig);
-
-export default db;
+});
+export const auth = firebaseApp.auth();
+export const db = firebaseApp.firestore();
+export default firebaseApp;
