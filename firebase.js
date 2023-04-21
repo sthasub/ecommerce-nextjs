81af0573b0fb52b3811d351e9c40bf52193b409a
Subhash Shrestha <subhashshrestha3@gmail.com>
2023-04-21 13:34:23 +0930

    taking firebase config from .env file

--- firebase.js
+++ firebase.js
@@ -8,14 +8,7 @@ import { getFirestore} from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCh-TQC0r6hvf6sfQlznuXM5Qqhvv4SoZU",
-  authDomain: "ecommerce-nextjs-5b907.firebaseapp.com",
-  projectId: "ecommerce-nextjs-5b907",
-  storageBucket: "ecommerce-nextjs-5b907.appspot.com",
-  messagingSenderId: "947716126382",
-  appId: "1:947716126382:web:6af5f6d7a9f4e6523b5f3f"
-};
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 
 // Initialize Firebase or get existing firebase instance
 const app = !getApps().length? initializeApp(firebaseConfig):getApp();
